7a1bc5317ce788e74c9cb1092f8b72b1aa00e636
ethanp <ethanp.dev@gmail.com>
2023-08-15 20:55:13 +0700

    Adding unit tests

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -10,6 +10,10 @@ export function ApiStack({ stack, app }: StackContext) {
       authorizer: "iam",
       function: {
         bind: [table],
+        environment: {
+          // @ts-ignore
+          STRIPE_SECRET_KEY: process?.env?.STRIPE_SECRET_KEY,
+        },
       },
     },
     routes: {
@@ -18,6 +22,7 @@ export function ApiStack({ stack, app }: StackContext) {
       "GET /notes": "packages/functions/src/list.main",
       "PUT /notes/{id}": "packages/functions/src/update.main",
       "DELETE /notes/{id}": "packages/functions/src/delete.main",
+      "POST /billing": "packages/functions/src/billing.main",
     },
   });
 
